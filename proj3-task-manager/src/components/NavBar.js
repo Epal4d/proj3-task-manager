73bd2015e36b92cf211f1c0f548406e9e0810b8d
evan.al4d <evan.al4d@gmail.com>
2025-04-07 03:31:12 -0500

    add routes to app page

--- proj3-task-manager/src/components/NavBar.js
+++ proj3-task-manager/src/components/NavBar.js
@@ -2,13 +2,15 @@ import { link } from "react-router-dom"
 
 const NavBar = () => {
     return (
+        <>
         <div>
             <nav>
-                <link to="/"> Home </link>
-                <link to="/">About</link>
-                <link to="/">Contact Us</link>
+                <Link to="/"> Home </Link>
+                <Link to="/About">About</Link>
+                <Link to="/Contact">Contact Us</Link>
             </nav>
         </div>
+        </>
     )
 }
 
